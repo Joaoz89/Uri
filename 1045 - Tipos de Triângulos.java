40ec43aa811b5cde7f10fce4caac48968b0318fa
Joaoz89 <113314996+Joaoz89@users.noreply.github.com>
2023-07-03 21:43:30 -0300

    Update and rename uri to 1045 - Tipos de Triângulos.java

---
+++ 1045 - Tipos de Triângulos.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main1042 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+	   double A, B, C, aux;
+	   
+	   A = sc.nextDouble();
+	   B = sc.nextDouble();
+	   C = sc.nextDouble();
+	
+	   if (A < B) {
+		   aux = A;
+		   A = B;
+		   B = aux;
+	   }
+	   if (A < C) {
+		   aux = A;
+		   A = C;
+		   C = aux;
+	   }
+	   if (B > C) {
+		   aux = B;
+		   B = C;
+		   C = aux;
+	   }
+	   if (A >= B + C) {
+		   System.out.println("NAO FORMA UM TRIANGULO");
+	   }else {
+		   if (A == B && A == C)
+			   System.out.println("TRIANGULO EQUILATERO");
+		   if (A * A == (B * B + C * C)) 
+			   System.out.println("TRIANGULO RETANGULO");
+		   if (A * A > (B * B + C * C)) 
+				System.out.println("TRIANGULO OBTUSANGULO");
+		   if (A * A < (B * B + C * C))
+			   System.out.println("TRIANGULO ACUTANGULO");
+		   if (A == B && !(B == C) || A == C && !(C == B) || C == B && !(B == A))
+			   System.out.println("TRIANGULO ISOSCELES");
+	   }
+	   /*
+	    * double tempA = Math.max(A, Math.max(B, C));
+		double tempB = 0;
+		double tempC = 0;
+		
+		if (tempA == A) {
+			tempB =Math.max(B, C);
+			tempC =Math.min(B, C);
+		}
+		if (tempA == B) {
+			tempB =Math.max(A, C);
+			tempC =Math.min(A, C);
+		}
+		if (tempA == C) {
+			tempB =Math.max(B, A);
+			tempC =Math.min(B, A);
+		}
+		-----
+		if (tempA >= (tempB + tempC)) {
+			System.out.print("NAO FORMA TRIANGULO\n");
+	    */
+		
+	    sc.close();
+
+		}
+}
