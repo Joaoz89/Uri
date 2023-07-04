fff3b2d2f1fc88a8c8298b66e5f3f00a29c382b5
Joaoz89 <113314996+Joaoz89@users.noreply.github.com>
2023-07-03 22:03:19 -0300

    Update 1171 -  Frequencia de numeros.java

--- 1171 -  Frequencia de numeros.java
+++ 1171 -  Frequencia de numeros.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
-public class Main1046 {
+public class Main {
 
 	public static void main(String[] args) {		
 		Scanner leitor = new Scanner(System.in);
